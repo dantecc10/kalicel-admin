2cef9d4cc8d7a2584c56ad59cde256cdb7ef551d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-28 22:13:50 -0600

    generalización de la función de altas y bajas

--- assets/js/AltasBajas.js
+++ assets/js/AltasBajas.js
@@ -1,7 +1,18 @@
-var claveObjetivo = 0;
 function bajaCantidad(claveObjetivo) {
-    var spanObjetivo = ("cantidad" + claveObjetivo);
-    var cantidadObjetivoActual = parseInt(document.getElementById(spanObjetivo).outerText);
+    var spanObjetivo = "";
+    var cantidadObjetivoActual = 0;
+    spanObjetivo = ("cantidad" + claveObjetivo);
+    cantidadObjetivoActual = parseInt(document.getElementById(spanObjetivo).outerText);
 
-    document.getElementById(spanObjetivo).innerHTML = cantidadObjetivoActual++;
+    switch (operación) {
+        case "alta":
+            document.getElementById(spanObjetivo).innerHTML = (cantidadObjetivoActual++);
+            break;
+        case "baja":
+            document.getElementById(spanObjetivo).innerHTML = (cantidadObjetivoActual--);
+            break;
+        default:
+            console.error("Error al recibir el valor del parámetro 'operación'.")
+            break;
+    }
 }
