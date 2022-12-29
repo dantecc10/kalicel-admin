2961fb3ae3a066bceb692d7f90df561f2bbf6d9c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-28 23:03:38 -0600

    cada vez más cerca

--- assets/js/AltasBajas.js
+++ assets/js/AltasBajas.js
@@ -1,19 +1,19 @@
-// var operación;
-// var claveObjetivo;
-function bajaAltaCantidad(claveObjetivo, operación) {
-    var spanObjetivo = "";
-    var cantidadObjetivoActual = 0;
-    spanObjetivo = ("cantidad" + claveObjetivo);
-    cantidadObjetivoActual = parseInt(document.getElementById(spanObjetivo).outerText);
+function bajaAltaCantidadDebug(claveObjetivo, operación) {
+    //var spanObjetivo = "";
+    //var cantidadObjetivoActual = 0;
+    var spanObjetivo = ("cantidad" + claveObjetivo);
+    var cantidadObjetivoActual = parseInt(document.getElementById(spanObjetivo).outerText);
     console.log("La cantidad " + claveObjetivo + " actual es de " + cantidadObjetivoActual);
 
     switch (operación) {
         case "alta":
+            document.getElementById(spanObjetivo).innerHTML = "";
             document.getElementById(spanObjetivo).innerHTML = (cantidadObjetivoActual++);
-            console.log("Se solicitó un alta en la cantidad " + claveObjetivo);
+            console.log("Se solicitó un alta en la cantidad " + claveObjetivo + "a " + (cantidadObjetivoActual++));
             break;
         case "baja":
-            document.getElementById(spanObjetivo).innerHTML = (cantidadObjetivoActual--);
+            document.getElementById(spanObjetivo).innerHTML = "";
+            console.log("Se solicitó un alta en la cantidad " + claveObjetivo + "a " + (cantidadObjetivoActual--));
             console.log("Se solicitó una baja en la cantidad " + claveObjetivo);
             break;
         default:
