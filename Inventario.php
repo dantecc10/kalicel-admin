23b9ab21f683994a3a014d92eccd5ccdfa581258
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-28 21:48:38 -0600

    título de inventario

--- Inventario.php
+++ Inventario.php
@@ -13,7 +13,7 @@ include "php scripts/Conexión.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Table - Kalicel</title>
+    <title>Inventario - Kalicel</title>
     <meta name="description" content="Sistema de administración web de reparaciones, estados de pedidos, e interacción con el cliente.">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
