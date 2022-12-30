08250008f4e3694dc324af33f2682ba0a536c692
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-29 23:46:54 -0600

    probamos con el innerhtml

--- assets/js/AltasBajas.js
+++ assets/js/AltasBajas.js
@@ -2,7 +2,7 @@ function bajaAltaCantidad(claveObjetivo, operación) {
     //var spanObjetivo = "";
     //var cantidadObjetivoActual = 0;
     var spanObjetivo = ("cantidad" + claveObjetivo);
-    var cantidadObjetivoActual = /*parseInt*/(document.getElementById(spanObjetivo).outerText);
+    var cantidadObjetivoActual = parseInt(document.getElementById(spanObjetivo).innerHTML);
     console.log("La cantidad " + claveObjetivo + " actual es de " + cantidadObjetivoActual);
 
     switch (operación) {
@@ -13,8 +13,8 @@ function bajaAltaCantidad(claveObjetivo, operación) {
             break;
         case "baja":
             document.getElementById(spanObjetivo).innerHTML = "";
-            document.getElementById(spanObjetivo).innerHTML = (cantidadObjetivoActual--);
-            console.log("Se solicitó una baja en la cantidad " + claveObjetivo + "a: " + (cantidadObjetivoActual++));
+            document.getElementById(spanObjetivo).innerHTML = (cantidadObjetivoActual - 1);
+            console.log("Se solicitó una baja en la cantidad " + claveObjetivo + "a: " + (cantidadObjetivoActual - 1));
             break;
         default:
             console.error("Error al recibir el valor del parámetro 'operación'.");
