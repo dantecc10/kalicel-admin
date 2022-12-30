7ac1c363ebb80c4ff2d05b7b5fac225764a23ca2
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-29 23:24:57 -0600

    veamos que

--- php scripts/verificaSQL.php
+++ php scripts/verificaSQL.php
@@ -11,7 +11,9 @@ function Conectarse()
         echo "Error conectando a la base de datos. Probemos con XAMPP";
         $conexión = new mysqli("127.0.0.1", "root", "", "kalicel");
         $consulta = "SELECT * FROM `displays` WHERE `id_display` = 2";
-        $consulta = mysqli_query($conexión, $consulta) or die("No funcionó lo de XAMPP");
+        $resultado = mysqli_query($conexión, $consulta) or die("No funcionó lo de XAMPP");
+        $columna = mysqli_fetch_all($resultado);
+        echo $columna['id_display'];
         exit();
     }
     #if (!mysqli_select_db($nombredb, $link)) {
