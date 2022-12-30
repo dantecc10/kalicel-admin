edb4b8f475927ea6f6393276b119d99a37748b90
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 00:38:20 -0600

    error en las cantidades

--- assets/js/AltasBajas.js
+++ assets/js/AltasBajas.js
@@ -40,14 +40,14 @@ function bajaAltaCantidad(claveObjetivo, operación) {
             console.log("Se solicitó un alta en la cantidad " + claveObjetivo + "a: " + (cantidadObjetivoActual + 1));
             document.getElementById(spanObjetivo).innerHTML = ("" + (cantidadObjetivoActual + 1));
 
-            bajaAltaSQL(claveObjetivo, claveObjetivo, "alta");
+            bajaAltaSQL(claveObjetivo, cantidadObjetivoActual, "alta");
             break;
         case "baja":
             // document.getElementById(spanObjetivo).innerHTML = "";
             console.log("Se solicitó una baja en la cantidad " + claveObjetivo + "a: " + (cantidadObjetivoActual - 1));
             document.getElementById(spanObjetivo).innerHTML = ("" + (cantidadObjetivoActual - 1));
 
-            bajaAltaSQL(claveObjetivo, claveObjetivo, "baja");
+            bajaAltaSQL(claveObjetivo, cantidadObjetivoActual, "baja");
             break;
         default:
             console.error("Error al recibir el valor del parámetro 'operación'.");
