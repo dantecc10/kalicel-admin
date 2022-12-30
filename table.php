da203efe16afb4e1e48bbe04887ee6a2dac38ca7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 11:05:45 -0600

    devolviendo los estilos

--- table.php
+++ table.php
@@ -13,7 +13,8 @@
     <meta name="description" content="Sistema de administración web de reparaciones, estados de pedidos, e interacción con el cliente.">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
-    <link id="mainCSS" rel="stylesheet" href="https://kalicel.castelancarpinteyro.club/admin/assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 </head>
 
