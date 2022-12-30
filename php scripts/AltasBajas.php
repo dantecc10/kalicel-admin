8743794a204e9b30d23a2862402a6cce9f02608c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 11:39:07 -0600

    w3schools

--- php scripts/AltasBajas.php
+++ php scripts/AltasBajas.php
@@ -19,9 +19,29 @@ switch ($operación) {
         break;
 }
 // Actualización de la cantidad de displays
-$consulta = ("UPDATE `displays` SET `cantidad_display` = $nuevaCantidad WHERE `id_display`= " . $id);
+$consulta = ("UPDATE displays SET cantidad_display = " . $nuevaCantidad . " WHERE id_display= " . $id);
+
+# $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
 echo $consulta;
+
+if ($conexión->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $resultado = mysqli_query($conexión, $consulta) or die("No se ejecutó la çonsulta de actualización...");
+
+
+
+if ($conexión->query($consulta) === TRUE) {
+    echo "Actualización exitosa";
+} else {
+    echo "Error en la actualización del registro: : " . $conexión->error;
+}
+
+$conexión->close();
+
+
 
 // Registro de edición en la actividad de usuarios (historial de seguridad)
 # $consulta = ("UPDATE `displays` SET `cantidad_display` = $nuevaCantidad WHERE `id_display`= " . $id);
