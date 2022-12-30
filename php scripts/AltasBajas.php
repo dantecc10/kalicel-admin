cd761f39af8c6fc18b8d95d6db39c9041ad5d7c6
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 14:38:28 -0600

    prueba inicial de registro dinámico de operaciones

--- php scripts/AltasBajas.php
+++ php scripts/AltasBajas.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (empty($_SESSION['ID'])) {
+    header("Location: login.php");
+}
 include "php scripts/Conexión.php";
 
 $id = $_GET['id'];
@@ -6,14 +10,26 @@ $cantidad = $_GET['cantidad'];
 $operación = strval($_GET['operación']);
 # $usuario = strval($_GET['usuario']);
 
+$autor = $_SESSION['Nombre'];
+
+$consulta = "SELECT * FROM `displays` WHERE `id_display` = $id";
+$resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
+
+while ($columna = mysqli_fetch_array($resultado)) {
+    $datosDisplay['Marca'] = $columna['modelo_display'];
+    $datosDisplay['Modelo'] = $columna['marca_display'];
+    $datosDisplay['Color'] = $columna['color_display'];
+}
+
+
 switch ($operación) {
     case 'alta':
         $nuevaCantidad = ($cantidad + 1);
-        $registraOperación = crearOperación($id, "alta", $_SESSION['Nombre']);
+        $registraOperación = crearOperación($id, "alta", $autor, $datosDisplay['Marca'], $datosDisplay['Modelo'], $datosDisplay['Color']);
         break;
     case 'baja':
         $nuevaCantidad = ($cantidad - 1);
-        $registraOperación = crearOperación($id, "baja", $_SESSION['Nombre']);
+        $registraOperación = crearOperación($id, "baja", $autor, $datosDisplay['Marca'], $datosDisplay['Modelo'], $datosDisplay['Color']);
         break;
     default:
         header("location: FallaSwitchActualización.php");
@@ -22,9 +38,10 @@ switch ($operación) {
 // Actualización de la cantidad de displays
 $modificaCantidad = ("UPDATE displays SET cantidad_display = $nuevaCantidad WHERE id_display = $id");
 
-function crearOperación($clave, $acción, $autor)
+function crearOperación($clave, $acción, $autor, $marca, $modelo, $color)
 {
-    return "INSERT INTO `operaciones` VALUES('', 'Prueba de script', 'Estoy insertando operaciones de prueba', '$autor', current_timestamp)";
+    $sql = ("INSERT INTO `operaciones` VALUES ('', '" . ucfirst($acción) . "', 'Baja de $modelo - $marca (display); color " . lcfirst($color) . ".', '$autor', current_timestamp)");
+    return $sql; //"INSERT INTO `operaciones` VALUES('', 'Prueba de script', 'Estoy insertando operaciones de prueba', '$autor', current_timestamp)";
 }
 
 
@@ -33,7 +50,6 @@ $conexión = new mysqli("localhost", "kalicel", "kalicelrepair", "kalicel");
 if ($conexión->connect_error) {
     die("Connection failed: " . $conexión->connect_error);
 }
-
 
 $conexión->query($modificaCantidad);
 $conexión->query($registraOperación);
