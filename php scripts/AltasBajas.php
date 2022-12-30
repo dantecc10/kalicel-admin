ff8bcba4c6f9770908b40975b13bee4dc83cb8c3
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 14:51:32 -0600

    intento

--- php scripts/AltasBajas.php
+++ php scripts/AltasBajas.php
@@ -3,7 +3,7 @@ session_start();
 #if (empty($_SESSION['ID'])) {
 #    header("Location: ../login.php");
 #}
-include "php scripts/Conexión.php";
+include "Conexión.php";
 
 $id = $_GET['id'];
 $cantidad = $_GET['cantidad'];
