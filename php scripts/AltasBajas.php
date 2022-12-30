11b91390e24a280cb23e0dbce54aaf97da14eadf
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 14:11:18 -0600

    veamos qué sucede

--- php scripts/AltasBajas.php
+++ php scripts/AltasBajas.php
@@ -9,33 +9,40 @@ $operación = strval($_GET['operación']);
 switch ($operación) {
     case 'alta':
         $nuevaCantidad = ($cantidad + 1);
+        $registraOperación = crearOperación($id, "alta", $_SESSION['Nombre']);
         break;
     case 'baja':
         $nuevaCantidad = ($cantidad - 1);
+        $registraOperación = crearOperación($id, "baja", $_SESSION['Nombre']);
         break;
-
     default:
         header("location: FallaSwitchActualización.php");
         break;
 }
 // Actualización de la cantidad de displays
-$consulta = ("UPDATE displays SET cantidad_display = $nuevaCantidad WHERE id_display = $id");
+$modificaCantidad = ("UPDATE displays SET cantidad_display = $nuevaCantidad WHERE id_display = $id");
 
-# $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
-#mysqli_query($conexión, $consulta);
+function crearOperación($clave, $acción, $autor)
+{
+    return "INSERT INTO `operaciones` VALUES('', 'Prueba de script', 'Estoy insertando operaciones de prueba', '$autor', current_timestamp)";
+}
+
+
 $conexión = new mysqli("localhost", "kalicel", "kalicelrepair", "kalicel");
 
 if ($conexión->connect_error) {
     die("Connection failed: " . $conexión->connect_error);
 }
 
-$conexión->query($consulta);
-#echo $consulta;
+
+$conexión->query($modificaCantidad);
+$conexión->query($registraOperación);
+#echo $modificaCantidad;
 
 
-//$resultado = mysqli_query($conexión, $consulta) or die("No se ejecutó la çonsulta de actualización...");
+//$resultado = mysqli_query($conexión, $modificaCantidad) or die("No se ejecutó la çonsulta de actualización...");
 
-if ($conexión->query($consulta) === TRUE) {
+if ($conexión->query($modificaCantidad) === TRUE) {
     #echo "Actualización exitosa";
 } else {
     #echo "Error en la actualización del registro: : " . $conexión->error;
@@ -47,6 +54,6 @@ echo ($nuevaCantidad);
 
 
 // Registro de edición en la actividad de usuarios (historial de seguridad)
-# $consulta = ("UPDATE `displays` SET `cantidad_display` = $nuevaCantidad WHERE `id_display`= " . $id);
-# echo $consulta;
-# $resultado = mysqli_query($conexión, $consulta) or die("No se ejecutó la çonsulta de actualización...");
+# $modificaCantidad = ("UPDATE `displays` SET `cantidad_display` = $nuevaCantidad WHERE `id_display`= " . $id);
+# echo $modificaCantidad;
+# $resultado = mysqli_query($conexión, $modificaCantidad) or die("No se ejecutó la çonsulta de actualización...");
