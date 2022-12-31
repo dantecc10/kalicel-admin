7b629010600aa73e0d306c38c801e9ca9baa7bca
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-12-30 18:15:35 -0600

    sin ícono

--- Inventario.php
+++ Inventario.php
@@ -33,7 +33,7 @@ include "php scripts/Conexión.php";
     <div id="wrapper">
         <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="background: #fe0000;">
             <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-                    <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
+                    <div class="sidebar-brand-icon rotate-n-15"></div>
                     <div class="sidebar-brand-text mx-3"><span>Kalicel</span></div>
                 </a>
                 <hr class="sidebar-divider my-0">
