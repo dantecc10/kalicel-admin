128d25607ccad6059bd35412ab0d630133e12182
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-02 18:58:44 -0600

    a ver así

--- Inventario.php
+++ Inventario.php
@@ -238,12 +238,13 @@ include "php scripts/Conexión.php";
                 </div>
             </div>
         </div>
-        <footer class="bg-white sticky-footer">
-            <div class="container my-auto">
-                <div class="text-center my-auto copyright"><span>Copyright © Kalicel 2022</span></div>
-            </div>
-        </footer>
-    </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+    </div>
+    <footer class="bg-white sticky-footer">
+        <div class="container my-auto">
+            <div class="text-center my-auto copyright"><span>Copyright © Kalicel 2022</span></div>
+        </div>
+    </footer>
+    <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/script.min.js"></script>
 </body>
