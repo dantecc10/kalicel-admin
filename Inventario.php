e25d4d297077ef769d6f3f40a1a40f97e8cd4c07
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-29 14:25:04 -0600

    2023

--- Inventario.php
+++ Inventario.php
@@ -246,7 +246,7 @@ include "php scripts/Conexión.php";
     </div>
     <footer class="bg-white sticky-footer">
         <div class="container my-auto">
-            <div class="text-center my-auto copyright"><span>Copyright © Kalicel 2022</span></div>
+            <div class="text-center my-auto copyright"><span>Copyright © Kalicel 2023</span></div>
         </div>
     </footer>
     <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
