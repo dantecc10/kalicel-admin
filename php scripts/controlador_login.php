a433ccd352693f5fadebc4ad2d382bc73cbe222e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-29 00:40:15 -0600

    conexión directa

--- php scripts/controlador_login.php
+++ php scripts/controlador_login.php
@@ -3,8 +3,8 @@ if (!empty($_POST['InicioSesión'])) {
     if (!empty($_POST['email']) and !empty($_POST['password'])) {
         $email = $_POST['email'];
         $password = $_POST['password'];
-        include "php scripts/Conexión.php";
-
+        #include "php scripts/Conexión.php";
+        $conexión = new mysqli("localhost", "kalicel", "kalicelrepair", "kalicel"); 
         $sql = $conexión->query("SELECT * FROM `usuarios` WHERE `email_usuario`='$email' AND `contraseña_usuario`='$password'");
         if ($datos = $sql->fetch_object()) {
             session_start();
