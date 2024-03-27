841b1099563b3c9a657b9f82c698d0e8251117e0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-27 10:33:30 -0600

    Avances en frontend y tabla de reparaciones

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -2,15 +2,16 @@
 function changeText(idsql, estado) {
     switch (estado) {
         case 'pendiente':
-            var span = document.getElementById('status' + idsql);
+            var span = document.getElementById('status-' + idsql);
             span.textContent = "Listo";
+            span.setAttribute("onmouseout", "restaurarValor()");
             break;
         case 'listo':
-            var span = document.getElementById('status' + idsql);
+            var span = document.getElementById('status-' + idsql);
             span.textContent = "Entregado";
             break;
         case 'entregado':
-            var span = document.getElementById('status' + idsql);
+            var span = document.getElementById('status-' + idsql);
             span.textContent = "Pendiente";
             break;
 
