c85db6c3bf838810a0cc4cd5f580cdffeb7b6430
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-29 04:08:47 -0600

    comments

--- php scripts/add-fix-order.php
+++ php scripts/add-fix-order.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['ID'])) {
 
     include_once "connection.php";
     $stmt = $connection->prepare("INSERT INTO `fix_orders` VALUES ('', ?, ?, 1, ?, ?, ?, ?, CURDATE(), CURTIME(), ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssiddsss", $_POST["brand"], $_POST["model"], $_POST["customer"], $_POST["mobile"], $_POST["email"], $_SESSION['ID'], $_POST["cost"], $_POST["paid_amount"], $_POST["fail"], $_POST["work"], $_POST["coments"]);
+    $stmt->bind_param("sssssiddsss", $_POST["brand"], $_POST["model"], $_POST["customer"], $_POST["mobile"], $_POST["email"], $_SESSION['ID'], $_POST["cost"], $_POST["paid_amount"], $_POST["fail"], $_POST["work"], $_POST["comments"]);
 
     // Ejecutar la consulta preparada
     if ($stmt->execute()) {
