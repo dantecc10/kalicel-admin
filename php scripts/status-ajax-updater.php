d87bf526cd39dccb1341398189b6c8227ceb2416
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-29 11:53:49 -0600

    suggestions

--- php scripts/status-ajax-updater.php
+++ php scripts/status-ajax-updater.php
@@ -6,20 +6,21 @@ if (empty($_SESSION['ID'])) {
     include_once "functions.php";
     include_once "connection.php";
 
-    $stmt = $connection->prepare("SELECT `status_fix_order` FROM `fix_orders` WHERE `id_fix_order` = ?;");
+    $stmt = $connection->prepare("SELECT `status_fix_order` FROM `fix_orders` WHERE `id_fix_order` = ?");
     $stmt->bind_param("i", $_POST['target_id']);
 
     // Ejecutar la consulta preparada
     if ($stmt->execute()) {
         $result = $stmt->get_result();
         if ($result->num_rows == 1) {
-            // El registro existe, obtener el valor del status
+            // El registro existe, obtener el valor del estado
             $row = $result->fetch_assoc();
             $status = $row['status_fix_order'];
 
-            $stmt_update = $connection->prepare("UPDATE `fix_orders` SET (`status_fix_order` = ?) WHERE (`id_fix_order` = ?);");
+            // Actualizar el estado
+            $stmt_update = $connection->prepare("UPDATE `fix_orders` SET `status_fix_order` = ? WHERE `id_fix_order` = ?");
             $status = ($status == 3) ? 1 : ++$status;
-            $stmt_update->bind_param("ii", $_POST['target_id'], $status);
+            $stmt_update->bind_param("ii", $status, $_POST['target_id']);
             echo ($stmt_update->execute()) ? "success" : "error";
         } else {
             echo "error";
@@ -28,8 +29,8 @@ if (empty($_SESSION['ID'])) {
         echo "error";
     }
 
-
     // Cerrar la conexión
     $stmt->close();
+    $stmt_update->close();
     $connection->close();
 }
