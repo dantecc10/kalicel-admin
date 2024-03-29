699c4803876d6c91f6dccc872e4e21249b61f09c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-29 11:46:48 -0600

    Working on PHP reponse for AJAX

--- php scripts/status-ajax-updater.php
+++ php scripts/status-ajax-updater.php
@@ -2,5 +2,35 @@
 session_start();
 if (empty($_SESSION['ID'])) {
     header("Location: login.php");
+} else {
+    include_once "functions.php";
+    include_once "connection.php";
+
+    $stmt = $connection->prepare("SELECT `status_fix_order` FROM `fix_orders` WHERE `id_fix_order` = ?;");
+    $stmt->bind_param("i", $_POST['target_id']);
+
+    // Ejecutar la consulta preparada
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        if ($result->num_rows == 1) {
+            // El registro existe, obtener el valor del status
+            $row = $result->fetch_assoc();
+            $status = $row['status_fix_order'];
+
+            $stmt_update = $connection->prepare("UPDATE `fix_orders` SET (`status_fix_order` = ?) WHERE (`id_fix_order` = ?);");
+            $status = ($status == 3) ? 1 : $status++;
+            $stmt_update->bind_param("ii", $_POST['target_id'], $status);
+            $msg = ($stmt_update->execute()) ? "success" : "error";
+            echo $msg;
+        } else {
+            echo "error";
+        }
+    } else {
+        echo "error";
+    }
+
+
+    // Cerrar la conexión
+    $stmt->close();
+    $connection->close();
 }
-include_once "functions.php";
