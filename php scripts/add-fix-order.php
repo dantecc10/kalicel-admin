8363007e2dd7d0efca7b630a80f0ae298dd0fcc0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-29 04:03:38 -0600

    let's try

--- php scripts/add-fix-order.php
+++ php scripts/add-fix-order.php
@@ -2,4 +2,18 @@
 session_start();
 if (!isset($_SESSION['ID'])) {
     header("Location: ../login.php");
+} else {
+
+    $stmt = $conn->prepare("INSERT INTO `fix_orders` VALUES ('', ?, ?, 1, ?, ?, ?, ?, CURRDATE(), CURRTIME(), ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssiddsss", $_POST["brand"], $_POST["model"], $_POST["customer"], $_POST["mobile"], $_POST["email"], $_SESSION['ID'], $_POST["cost"], $_POST["paid_amount"], $_POST["fail"], $_POST["work"], $_POST["coments"]);
+
+    // Ejecutar la consulta preparada
+    if ($stmt->execute()) {
+        echo "Inserción exitosa";
+    } else {
+        echo "Error al insertar datos: " . $stmt->error;
+    }
+
+    // Cerrar la conexión
+    $stmt->close();
 }
