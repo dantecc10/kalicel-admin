37942f4a9c0b042251d9bbc9be55eef57081fb66
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-03-29 04:11:55 -0600

    rebuilding navbar

--- Reparaciones.php
+++ Reparaciones.php
@@ -50,21 +50,97 @@ include_once "php scripts/functions.php";
         <div class="d-flex flex-column" id="content-wrapper">
             <div id="content">
                 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
-                    <div class="container-fluid"><button class="btn btn-link btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
-                        <form class="d-none d-sm-inline-block my-2 my-md-0 mw-100 navbar-search col-12">
-                            <div class="input-group col-12"><input class="bg-light form-control border-0 small" type="text" placeholder="Buscar">
-                                <div class="input-group-append"><button class="btn btn-primary py-0" type="button" style="background-color: var(--bs-kalicel-rojo);"><i class="fas fa-search"></i></button></div>
-                            </div>
+                    <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                        <form class="d-none d-sm-inline-block me-auto ms-md-3 my-2 my-md-0 mw-100 navbar-search">
+                            <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Buscar"><button class="btn btn-primary py-0" type="button" style="background-color: var(--bs-kalicel-rojo);"><i class="fas fa-search"></i></button></div>
                         </form>
-                        <ul class="navbar-nav flex-nowrap ms-auto m-0">
-                            <li class="nav-item dropdown show d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="true" data-bs-toggle="dropdown" href="#"></a>
-                                <div class="dropdown-menu show dropdown-menu-end p-3 animated--grow-in" data-bs-popper="none" aria-labelledby="searchDropdown"></div>
+                        <ul class="navbar-nav flex-nowrap ms-auto">
+                            <li class="nav-item dropdown show d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+                                <div class="dropdown-menu show dropdown-menu-end p-3 animated--grow-in" data-bs-popper="none" aria-labelledby="searchDropdown">
+                                    <form class="me-auto navbar-search w-100">
+                                        <div class="input-group"><input class="form-control bg-light border-0 small" type="text" placeholder="Buscar...">
+                                            <div class="input-group-text input-group-append"><button class="btn btn-primary py-0" type="button" style="background-color: var(--bs-kalicel-rojo);"><i class="fas fa-search"></i></button></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown no-arrow mx-1">
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" href="#" aria-expanded="false" data-bs-toggle="dropdown"><span class="badge bg-danger badge-counter">3+</span><i class="fas fa-bell"></i></a>
+                                    <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
+                                        <h6 class="dropdown-header">Heading</h6><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="me-3">
+                                                <div class="bg-primary icon-circle"><i class="far fa-star far fa-file-alt text-white"></i></div>
+                                            </div>
+                                            <div><span class="small text-gray-500">Text</span>
+                                                <p>Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="me-3">
+                                                <div class="bg-primary icon-circle"><i class="far fa-star far fa-file-alt text-white"></i></div>
+                                            </div>
+                                            <div><span class="small text-gray-500">Text</span>
+                                                <p>Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="me-3">
+                                                <div class="bg-primary icon-circle"><i class="far fa-star far fa-file-alt text-white"></i></div>
+                                            </div>
+                                            <div><span class="small text-gray-500">Text</span>
+                                                <p>Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="me-3">
+                                                <div class="bg-primary icon-circle"><i class="far fa-star far fa-file-alt text-white"></i></div>
+                                            </div>
+                                            <div><span class="small text-gray-500">Text</span>
+                                                <p>Paragraph</p>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown no-arrow mx-1">
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" href="#" aria-expanded="false" data-bs-toggle="dropdown"><span class="badge bg-danger badge-counter">7</span><i class="fas fa-envelope"></i></a>
+                                    <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
+                                        <h6 class="dropdown-header">Heading</h6><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="dropdown-list-image me-3"><img class="rounded-circle" src="">
+                                                <div class="bg-success status-indicator"></div>
+                                            </div>
+                                            <div class="fw-bold">
+                                                <div class="text-truncate"><span>Text</span></div>
+                                                <p class="small text-gray-500 mb-0">Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="dropdown-list-image me-3"><img class="rounded-circle" src="">
+                                                <div class="bg-success status-indicator"></div>
+                                            </div>
+                                            <div class="fw-bold">
+                                                <div class="text-truncate"><span>Text</span></div>
+                                                <p class="small text-gray-500 mb-0">Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="dropdown-list-image me-3"><img class="rounded-circle" src="">
+                                                <div class="bg-success status-indicator"></div>
+                                            </div>
+                                            <div class="fw-bold">
+                                                <div class="text-truncate"><span>Text</span></div>
+                                                <p class="small text-gray-500 mb-0">Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="dropdown-list-image me-3"><img class="rounded-circle" src="">
+                                                <div class="bg-success status-indicator"></div>
+                                            </div>
+                                            <div class="fw-bold">
+                                                <div class="text-truncate"><span>Text</span></div>
+                                                <p class="small text-gray-500 mb-0">Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item d-flex align-items-center" href="#">Link<div class="dropdown-list-image me-3"><img class="rounded-circle" src="">
+                                                <div class="bg-success status-indicator"></div>
+                                            </div>
+                                            <div class="fw-bold">
+                                                <div class="text-truncate"><span>Text</span></div>
+                                                <p class="small text-gray-500 mb-0">Paragraph</p>
+                                            </div>
+                                        </a><a class="dropdown-item text-center small text-gray-500" href="#">Link</a>
+                                    </div>
+                                </div>
+                                <div class="shadow dropdown-list dropdown-menu dropdown-menu-end" aria-labelledby="alertsDropdown"></div>
                             </li>
                             <li class="nav-item dropdown no-arrow">
-                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="https://kalicel.castelancarpinteyro.club/admin/login.php" target="_blank"><span class="d-none d-lg-inline me-2 text-gray-600 small">Iniciar sesión&nbsp;<i class="far fa-user"></i></span></a>
-                                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log</a>
-                                        <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
-                                    </div>
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" href="#" aria-expanded="false" data-bs-toggle="dropdown"><span class="d-none d-lg-inline me-2 text-gray-600 small">Text</span><img class="rounded-circle img-fluid border img-profile" src="assets/img/avatars/3.png"></a>
+                                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#">Link<i class="far fa-star"></i></a><a class="dropdown-item" href="#">Link<i class="far fa-star"></i></a><a class="dropdown-item" href="#">Link<i class="far fa-star"></i></a><a class="dropdown-item" href="#">Link<i class="far fa-star"></i></a></div>
                                 </div>
                             </li>
                         </ul>
