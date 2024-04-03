45e775c21815ba0538ad5009bd3443695417a03f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-02 23:58:19 -0600

    ..

--- Reparaciones.php
+++ Reparaciones.php
@@ -28,7 +28,7 @@ include_once "php scripts/functions.php";
     <link rel="stylesheet" href="assets/css/extra.css">
 </head>
 
-<body id="page-top">
+<body id="page-top"> 
     <div id="wrapper">
         <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0" style="background: var(--bs-kalicel-rojo);">
             <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
