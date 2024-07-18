e1e6edb1dbe8f43efa91782b7d017cfc087c3f2e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-07-18 02:16:01 -0600

    update code

--- php scripts/controlador_login.php
+++ php scripts/controlador_login.php
@@ -15,13 +15,12 @@ if (!empty($_POST['InicioSesión'])) {
             $_SESSION['Email'] = $datos->email_usuario;
             header("location: ../Inventario.php");
         } else {
-            #echo "<div>Acceso denegado<div>";
             $mensajeDante = ("Hola Dante, he intentado iniciar con las siguientes credenciales al administrador de Kalicel y no he podido ingresar; ¿son correctas?:
             - Usuario: $email
             - Contraseña: $password");
             $mensajeDante = urlencode($mensajeDante);
             header("location: https://wa.me/527979773095?text=$mensajeDante");
-            #header("location: mailto:dantecc10@gmail.com");
+            #header("location: mailto:dantecandc10@gmail.com");
         }
     } else {
         header("Location: ../login.php");
