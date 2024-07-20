a93b71ecedcffd4238c67e7123f06cf4fe85f948
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-07-20 13:02:46 -0600

    dir

--- login.php
+++ login.php
@@ -35,6 +35,10 @@
                                     <!-- 
                                     action="php scripts/controlador_login.php"
                                     -->
+
+                                    <?php
+                                    echo __DIR__;
+                                    ?>
                                     <form class="user" method="POST" action="php scripts/controlador_login.php">
                                         <div class="mb-3"><input class="form-control form-control-user" type="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Ingrese su email" name="email"></div>
                                         <div class="mb-3"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Contraseña" name="password" value="current-password"></div>
