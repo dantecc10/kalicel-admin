7b0a143c07d93cececb75f1780ecd8863757d63a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-19 03:06:23 -0600

    updating code

--- php-scripts/search-pieces.php
+++ php-scripts/search-pieces.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$query = $_GET['query'];
 
 function onload_table()
 {
@@ -39,74 +38,68 @@ function onload_table()
 }
 
 # ConstruirTablaCarga();
-function ConstruirTablaBúsqueda($búsqueda)
+function search_displays($query)
 {
-    $conexión = mysqli_connect("localhost", "kalicel", "kalicelrepair", "kalicel");
+    include_once "connection.php";
     $comilla = '"';
-    $columnas = ["id_display", "modelo_display", "marca_display", "color_display", "cantidad_display", "versión_display", "caja_display"];
+    //$columnas = ["id_display", "modelo_display", "marca_display", "color_display", "cantidad_display", "calidad_display", "versión_display", "caja_display"];
+    $columnas = ["modelo_display", "marca_display", "cantidad_display", "calidad_display", "color_display", "versión_display"];
 
     // Diseño de lógica de filtros
+    $sql_query = "SELECT * FROM `displays` ";
     $where = "WHERE (";
-    $cuenta = count($columnas);
-    for ($i = 0; $i < $cuenta; $i++) {
-        $where .= $columnas[$i] . " LIKE '%" . $búsqueda . "%' OR ";
+    foreach ($columnas as $columna) {
+        $where .= "`$columna` LIKE '%?%' OR ";
     }
-    $where = substr_replace($where, "", -3);
-    $where .= ")";
+    $where = substr($where, 0, -3) . ");";
 
+    $sql_query .= $where;
+    $stmt = $connection->prepare($sql_query);
+    $stmt->bind_param("s", $query);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    $consulta = "SELECT * FROM `displays` $where";
-    #echo $consulta;
-    $resultado = mysqli_query($conexión, $consulta) or die("Error en la consulta a la base de datos");
-
-    echo ("<table class='table my-0' id='dataTable'>
-    <thead>
-    <tr>
-    <th>ID</th>
-    <th>Modelo</th>
-    <th>Marca</th>
-    <th>Color</th>
-            <th>Cantidad</th>
-            <th>Caja</th>
-            <th>Calidad</th>
-            <th>Versión</th>
+    $table_dom = ("
+    <table class='table my-0' id='dataTable'>
+        <thead>
+            <tr>
+                <th>Modelo</th>
+                <th>Marca</th>
+                <th>Cantidad</th>
+                <th>Calidad</th>
+                <th>Color</th>
+                <th>Versión</th>
             </tr>
-    </thead>
-    <tbody id='cuerpoTabla'>");
-    while ($columna = mysqli_fetch_array($resultado)) {
+        </thead>
+        <tbody id='cuerpoTabla'>");
 
-        echo "<tr>";
-        echo "<td>" . $columna['id_display'] . "</td>";
-        echo "<td>" . $columna['modelo_display'] . "</td>";
-        echo "<td>" . $columna['marca_display'] . "</td>";
-        echo "<td>" . $columna['color_display'] . "</td>";
-        echo "<td><button id='baja" . $columna['id_display'] . "' class='btn btn-primary btn-baja' onclick='javascript:bajaAltaCantidad(" . $columna['id_display'] . ", " . $comilla . "baja" . $comilla . ");' type='button'>-</button><span id='cantidad" . $columna['id_display'] .  "'>" . $columna['cantidad_display'] . "</span><button id='alta" . $columna['id_display'] . "' class='btn btn-primary btn-alta' onclick='javascript:bajaAltaCantidad(" . $columna['id_display'] . ", " . $comilla . "alta" . $comilla . ");' type='button'>+</button></td>";
-        echo "<td>" . $columna['caja_display'] . "</td>";
-        echo "<td>" . $columna['calidad_display'] . "</td>";
-        echo "<td>" . $columna['versión_display'] . "</td>";
-        echo "</tr>";
+    while ($row = $result->fetch_assoc()) {
+        $table_dom .= ("
+            <tr>
+                <td>$row[modelo_display]</td>
+                <td>$row[marca_display]</td>
+                <td>$row[cantidad_display]</td>
+                <td>$row[calidad_display]</td>
+                <td>$row[color_display]</td>
+                <td>$row[versión_display]</td>
+            </tr>");
     }
-    echo ("</tbody>
-    <tfoot>
-    <tr>
-    <td><strong>ID</strong></td>
-    <td><strong>Modelo</strong></td>
-    <td><strong>Marca</strong></td>
-    <td><strong>Color</strong></td>
-    <td><strong>Cantidad</strong></td>
-    <td><strong>Caja</strong></td>
-    <td>Calidad</td>
-    <td>Versión</td>
-    </tr>
-    </tfoot>
+    $table_dom .= ("
+        </tbody>
+        <tfoot>
+            <tr>
+                <td><strong>Modelo</strong></td>
+                <td><strong>Marca</strong></td>
+                <td><strong>Cantidad</strong></td>
+                <td><strong>Calidad</strong></td>
+                <td><strong>Color</strong></td>
+                <td><strong>Versión</strong></td>
+            </tr>
+        </tfoot>
     </table>");
-
-    mysqli_close($conexión);
+    $stmt->close();
+    $connection->close();
+    return $table_dom;
 }
 
-if ($búsqueda != "" ||  $búsqueda != null) {
-    ConstruirTablaBúsqueda($búsqueda);
-} else {
-    ConstruirTablaCarga($búsqueda);
-    #echo "Filtro desactivado";
-}
+echo (isset($_GET['query'])) ? search_displays($query) : onload_table();
